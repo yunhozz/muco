340a2ee2aeaf98e1d3d25e2aa7dcd00dd9a269ab
yunhozz <qkrdbsgh1121@naver.com>
2023-10-25 19:30:46 +0900

    feat: JWT 생성, 필터 관련 클래스 추가

---
+++ auth-service/src/main/java/com/muco/authservice/global/auth/jwt/JwtFilter.java
@@ -0,0 +1,48 @@
+package com.muco.authservice.global.auth.jwt;
+
+import io.jsonwebtoken.lang.Strings;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.Optional;
+
+@Slf4j
+@RequiredArgsConstructor
+public class JwtFilter extends OncePerRequestFilter {
+
+    private final JwtProvider jwtProvider;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String requestURI = request.getRequestURI();
+        String token = request.getHeader(HttpHeaders.AUTHORIZATION);
+        log.info("[Request URI] " + requestURI);
+
+        resolveToken(token).ifPresent(accessToken -> {
+            if (jwtProvider.isValidToken(accessToken)) {
+                Authentication authentication = jwtProvider.getAuthentication(accessToken);
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+            }
+        });
+
+        filterChain.doFilter(request, response);
+    }
+
+    private Optional<String> resolveToken(String token) {
+        return Strings.hasText(token) ? resolveParts(token) : Optional.empty();
+    }
+
+    private Optional<String> resolveParts(String token) {
+        String[] parts = token.split(" ");
+        return parts.length == 2 && parts[0].equals("Bearer") ? Optional.ofNullable(parts[1]) : Optional.empty();
+    }
+}
