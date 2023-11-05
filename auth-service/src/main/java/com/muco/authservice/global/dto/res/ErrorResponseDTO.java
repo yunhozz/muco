a5fa6718eab1f2f6c7093a446ea643c961c46ce1
yunhozz <qkrdbsgh1121@naver.com>
2023-11-05 16:57:11 +0900

    feat: 글로벌 예외 핸들러 추가, 관련 클래스 추가
    - UserExceptionHandler 추가
    - 에러 내용을 반환할 ErrorResponseDTO 생성
    - 대표적인 에러 내용을 담은 enum 생성

---
+++ auth-service/src/main/java/com/muco/authservice/global/dto/res/ErrorResponseDTO.java
@@ -0,0 +1,80 @@
+package com.muco.authservice.global.dto.res;
+
+import com.muco.authservice.global.enums.ErrorCode;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.validation.BindingResult;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class ErrorResponseDTO {
+
+    private int status;
+    private String code;
+    private String message;
+    private List<FieldErrorResponseDTO> fieldErrors;
+
+    private ErrorResponseDTO(ErrorCode errorCode) {
+        this.status = errorCode.getStatus();
+        this.code = errorCode.getCode();
+        this.message = errorCode.getMessage();
+        this.fieldErrors = new ArrayList<>();
+    }
+
+    private ErrorResponseDTO(ErrorCode errorCode, String message) {
+        this.status = errorCode.getStatus();
+        this.code = errorCode.getCode();
+        this.message = message;
+        this.fieldErrors = new ArrayList<>();
+    }
+
+    private ErrorResponseDTO(ErrorCode errorCode, List<FieldErrorResponseDTO> fieldErrors) {
+        ErrorResponseDTO errorResponseDTO = new ErrorResponseDTO(errorCode);
+        errorResponseDTO.fieldErrors = fieldErrors;
+    }
+
+    public static ErrorResponseDTO of(ErrorCode errorCode) {
+        return new ErrorResponseDTO(errorCode);
+    }
+
+    public static ErrorResponseDTO of(ErrorCode errorCode, String message) {
+        return new ErrorResponseDTO(errorCode, message);
+    }
+
+    public static ErrorResponseDTO of(ErrorCode errorCode, BindingResult result) {
+        return new ErrorResponseDTO(errorCode, FieldErrorResponseDTO.of(result));
+    }
+
+    @Getter
+    @AllArgsConstructor(access = AccessLevel.PRIVATE)
+    private static class FieldErrorResponseDTO {
+
+        private String field;
+        private String value;
+        private String reason;
+
+        private FieldErrorResponseDTO() {}
+
+        private static List<FieldErrorResponseDTO> of(String field, String value, String reason) {
+            return new ArrayList<>() {{
+                add(new FieldErrorResponseDTO(field, value, reason));
+            }};
+        }
+
+        private static List<FieldErrorResponseDTO> of(BindingResult result) {
+            return result.getFieldErrors().stream()
+                    .map(fieldError -> new FieldErrorResponseDTO(
+                            fieldError.getField(),
+                            fieldError.getRejectedValue() != null ? fieldError.getRejectedValue().toString() : "",
+                            fieldError.getDefaultMessage()
+                    ))
+                    .collect(Collectors.toList());
+        }
+    }
+}
