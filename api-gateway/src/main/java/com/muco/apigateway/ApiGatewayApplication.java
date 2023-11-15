ba06d84617c778c5f68fce25faeca8b4bfd74afb
yunhozz <qkrdbsgh1121@naver.com>
2023-11-15 21:49:15 +0900

    feat: OpenApi 초기 설정 추가

--- api-gateway/src/main/java/com/muco/apigateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/muco/apigateway/ApiGatewayApplication.java
@@ -1,13 +1,49 @@
 package com.muco.apigateway;
 
+import lombok.RequiredArgsConstructor;
+import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.gateway.route.RouteDefinition;
+import org.springframework.cloud.gateway.route.RouteDefinitionLocator;
+import org.springframework.context.annotation.Bean;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@RequiredArgsConstructor
 public class ApiGatewayApplication {
+
+	private final RouteDefinitionLocator locator;
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiGatewayApplication.class, args);
 	}
 
+	@Bean
+	public List<GroupedOpenApi> apis() {
+		List<GroupedOpenApi> groups = new ArrayList<>();
+		List<RouteDefinition> definitions = locator.getRouteDefinitions().collectList().block();
+		assert definitions != null;
+
+		definitions.stream()
+				.filter(definition -> definition.getId().matches(".*-service") || definition.getId().matches(".*-server"))
+				.forEach(definition -> {
+					String name = definition.getId()
+							.replaceAll("-service", "")
+							.replaceAll("-server", "");
+
+					GroupedOpenApi group = GroupedOpenApi.builder()
+							.pathsToMatch("/" + name + "/**")
+							.group(name)
+							.build();
+
+					groups.add(group);
+		});
+
+		return groups;
+	}
 }
