9cf86bc737b26643230200f7c4deaf2b2896d431
yunhozz <qkrdbsgh1121@naver.com>
2023-11-25 19:20:05 +0900

    feat: JWT 예외 핸들링 관련 클래스 추가

---
+++ auth-service/src/main/java/com/muco/authservice/global/auth/jwt/JwtAuthenticationEntryPoint.java
@@ -0,0 +1,22 @@
+package com.muco.authservice.global.auth.jwt;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Slf4j
+@Component
+public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
+
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException, ServletException {
+        log.error(exception.getLocalizedMessage());
+        response.sendError(HttpServletResponse.SC_UNAUTHORIZED);
+    }
+}
