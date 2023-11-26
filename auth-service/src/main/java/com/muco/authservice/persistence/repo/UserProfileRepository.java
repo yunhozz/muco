92b326dc3e3ce4adbfcff2c3bafd2d8ace5b411a
yunhozz <qkrdbsgh1121@naver.com>
2023-11-26 23:15:01 +0900

    fix: @Param 어노테이션 추가

--- auth-service/src/main/java/com/muco/authservice/persistence/repo/UserProfileRepository.java
+++ auth-service/src/main/java/com/muco/authservice/persistence/repo/UserProfileRepository.java
@@ -3,13 +3,14 @@ package com.muco.authservice.persistence.repo;
 import com.muco.authservice.persistence.entity.UserProfile;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
 public interface UserProfileRepository extends JpaRepository<UserProfile, Long> {
 
     @Query("select up from UserProfile up join fetch up.user u where up.email = :email")
-    Optional<UserProfile> findWithUserByEmail(String email);
+    Optional<UserProfile> findWithUserByEmail(@Param("email") String email);
 
     boolean existsUserProfileByEmail(String email);
 }
--- auth-service/src/main/java/com/muco/authservice/persistence/repo/UserRepository.java
+++ auth-service/src/main/java/com/muco/authservice/persistence/repo/UserRepository.java
@@ -3,6 +3,7 @@ package com.muco.authservice.persistence.repo;
 import com.muco.authservice.persistence.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
@@ -11,5 +12,5 @@ public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findById(Long userId);
 
     @Query("select u from UserProfile up join up.user u where up.email = :email")
-    Optional<User> findUserByEmail(String email);
+    Optional<User> findUserByEmail(@Param("email") String email);
 }
