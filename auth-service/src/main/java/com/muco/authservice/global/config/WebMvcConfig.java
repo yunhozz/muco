1e7ff416d4d99c7a7a9ae70f6e876015cfc6d937
yunhozz <qkrdbsgh1121@naver.com>
2023-11-26 21:26:39 +0900

    fix: CORS 옵션 수정
    * allowedOrigins -> allowedOriginPatterns

--- auth-service/src/main/java/com/muco/authservice/global/config/WebMvcConfig.java
+++ auth-service/src/main/java/com/muco/authservice/global/config/WebMvcConfig.java
@@ -13,7 +13,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry
                 .addMapping("/**")
-                .allowedOrigins("*")
+                .allowedOriginPatterns("*")
                 .allowedHeaders("*")
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE")
                 .allowCredentials(true)
