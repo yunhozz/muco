8f7eac5209ef9885b03b479b535254be3b80d6d2
yunhozz <qkrdbsgh1121@naver.com>
2023-11-26 22:32:27 +0900

    fix: 시큐리티 설정 정보 추가
    * 시큐리티 기본 제공 로그인 기능 비활성화
    * OAuth2 로그인 설정 정보 추가
    * 예외 핸들러 설정 정보 추가

--- auth-service/src/main/java/com/muco/authservice/global/config/SecurityConfig.java
+++ auth-service/src/main/java/com/muco/authservice/global/config/SecurityConfig.java
@@ -1,7 +1,12 @@
 package com.muco.authservice.global.config;
 
+import com.muco.authservice.global.auth.jwt.JwtAccessDeniedHandler;
+import com.muco.authservice.global.auth.jwt.JwtAuthenticationEntryPoint;
 import com.muco.authservice.global.auth.jwt.JwtFilter;
 import com.muco.authservice.global.auth.jwt.JwtProvider;
+import com.muco.authservice.global.auth.oauth.OAuth2AuthenticationFailureHandler;
+import com.muco.authservice.global.auth.oauth.OAuth2AuthenticationSuccessHandler;
+import com.muco.authservice.global.auth.oauth.OAuth2UserServiceImpl;
 import com.muco.authservice.global.auth.security.UserDetailsServiceImpl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
@@ -22,26 +27,40 @@ import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 @RequiredArgsConstructor
 public class SecurityConfig {
 
-    private final UserDetailsServiceImpl userDetailsService;
     private final JwtProvider jwtProvider;
+
+    private final UserDetailsServiceImpl userDetailsService;
+    private final OAuth2UserServiceImpl oAuth2UserService;
+
+    private final OAuth2AuthenticationSuccessHandler oAuth2AuthenticationSuccessHandler;
+    private final OAuth2AuthenticationFailureHandler oAuth2AuthenticationFailureHandler;
+
+    private final JwtAccessDeniedHandler jwtAccessDeniedHandler;
+    private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .cors(AbstractHttpConfigurer::disable)
                 .csrf(AbstractHttpConfigurer::disable)
-                .authorizeHttpRequests((authorizeRequest) -> {
-                    authorizeRequest
-                            .anyRequest().permitAll();
-                })
-                .headers((header) -> {
-                    header.frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin);
-                })
-                .sessionManagement((session) -> {
-                    session.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-                })
+                .authorizeHttpRequests(ar -> ar.anyRequest().permitAll())
+                .headers(h -> h.frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin))
+                .formLogin(AbstractHttpConfigurer::disable)
+                .httpBasic(AbstractHttpConfigurer::disable)
+                .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .addFilterBefore(new JwtFilter(jwtProvider), UsernamePasswordAuthenticationFilter.class)
                 .userDetailsService(userDetailsService)
-                .addFilterBefore(new JwtFilter(jwtProvider), UsernamePasswordAuthenticationFilter.class);
+                .oauth2Login(ol -> {
+                    ol.authorizationEndpoint(ep -> ep.baseUri("/oauth2/authorization"));
+                    ol.redirectionEndpoint(ep -> ep.baseUri("/oauth2/callback/*"));
+                    ol.userInfoEndpoint(ep -> ep.userService(oAuth2UserService));
+                    ol.successHandler(oAuth2AuthenticationSuccessHandler);
+                    ol.failureHandler(oAuth2AuthenticationFailureHandler);
+                })
+                .exceptionHandling(eh -> {
+                    eh.accessDeniedHandler(jwtAccessDeniedHandler);
+                    eh.authenticationEntryPoint(jwtAuthenticationEntryPoint);
+                });
 
         return http.build();
     }
