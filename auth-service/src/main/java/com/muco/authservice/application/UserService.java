6b573e5a1d3b5847696dc02f3cac483a33e05117
yunhozz <qkrdbsgh1121@naver.com>
2023-12-03 16:34:46 +0900

    fix: 회원가입 로직 수정
    * 회원가입 인증 메일 발송 도중 예외가 발생해도 트랜잭션이 커밋되도록 수정
    * 관련 예외 클래스 생성
    * html 수정

--- auth-service/src/main/java/com/muco/authservice/application/UserService.java
+++ auth-service/src/main/java/com/muco/authservice/application/UserService.java
@@ -1,5 +1,8 @@
 package com.muco.authservice.application;
 
+import com.muco.authservice.application.exception.MailDuplicateException;
+import com.muco.authservice.application.exception.MailSendFailException;
+import com.muco.authservice.application.exception.PasswordDifferentException;
 import com.muco.authservice.global.dto.req.SignUpRequestDTO;
 import com.muco.authservice.global.dto.res.SignUpResponseDTO;
 import com.muco.authservice.global.enums.LoginType;
@@ -10,12 +13,12 @@ import com.muco.authservice.persistence.entity.UserProfile;
 import com.muco.authservice.persistence.repo.UserPasswordRepository;
 import com.muco.authservice.persistence.repo.UserProfileRepository;
 import com.muco.authservice.persistence.repo.UserRepository;
-import jakarta.mail.Message;
 import jakarta.mail.MessagingException;
 import jakarta.mail.internet.MimeMessage;
 import lombok.RequiredArgsConstructor;
 import org.springframework.mail.MailException;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -34,17 +37,17 @@ public class UserService {
     private final JavaMailSender mailSender;
     private final BCryptPasswordEncoder encoder;
 
-    @Transactional(noRollbackFor = { MailException.class, MessagingException.class }) // 메일 전송에 실패해도 일단 DB 에 회원 정보 저장
+    @Transactional(noRollbackFor = MailSendFailException.class)
     public SignUpResponseDTO joinByEmail(SignUpRequestDTO dto) {
         String email = dto.getEmail();
         String password1 = dto.getPassword1();
         String password2 = dto.getPassword2();
 
         if (userProfileRepository.existsByEmail(email)) {
-            throw new RuntimeException("중복되는 이메일이 존재합니다. Email = " + email);
+            throw new MailDuplicateException("중복되는 이메일이 존재합니다. Email = " + email);
         }
         if (!password1.equals(password2)) {
-            throw new RuntimeException("비밀번호를 다시 한번 확인해주세요.");
+            throw new PasswordDifferentException("비밀번호를 다시 한번 확인해주세요.");
         }
 
         User user = new User(LoginType.LOCAL);
@@ -62,21 +65,22 @@ public class UserService {
         userProfileRepository.save(userProfile);
         userPasswordRepository.save(userPassword);
 
-        // 해당 이메일로 회원가입 인증 메일 발송
+        /* 회원가입 인증 메일 발송 */
         MimeMessage message = mailSender.createMimeMessage();
         String code = createCode();
         String text = makeVerifyingText(code);
 
         try {
-            message.setRecipients(Message.RecipientType.TO, email);
-            message.setSubject("[MUCO] 회원가입 인증 메일입니다.");
-            message.setText(text);
+            MimeMessageHelper messageHelper = new MimeMessageHelper(message, false, "UTF-8");
+            messageHelper.setTo(email);
+            messageHelper.setSubject("[MUCO] 회원가입 인증 메일입니다.");
+            messageHelper.setText(text, true);
             mailSender.send(message);
 
             RedisUtils.saveValue(email, code, Duration.ofHours(1)); // 인증 유효시간 1시간으로 설정
 
         } catch (MailException | MessagingException e) {
-            throw new RuntimeException("메일 전송에 실패하였습니다. 원인 : " + e.getLocalizedMessage());
+            throw new MailSendFailException("메일 전송에 실패하였습니다. 원인 : " + e.getLocalizedMessage());
         }
 
         return new SignUpResponseDTO(user.getId(), email);
@@ -99,7 +103,10 @@ public class UserService {
     }
 
     private static String makeVerifyingText(String code) {
-        return "<div style='margin:100px;'>" +
+        return "<!DOCTYPE html>" +
+                "<html>" +
+                "<body>" +
+                "<div style='margin:100px;'>" +
                 "<h1> 안녕하세요! MUCO 입니다. </h1>" +
                 "<br>" +
                 "<p>아래 코드를 회원가입 인증 페이지에서 입력해주세요<p>" +
@@ -110,6 +117,8 @@ public class UserService {
                 "<h3 style='color:blue;'>회원가입 인증 코드입니다.</h3>" +
                 "<div style='font-size:130%'>" +
                 "CODE : <strong>" + code + "</strong><div><br/> " +
-                "</div>";
+                "</div>" +
+                "</body>" +
+                "</html>";
     }
 }
