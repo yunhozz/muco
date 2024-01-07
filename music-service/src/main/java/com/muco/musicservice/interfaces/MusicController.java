0769850ad317cc14f90110058f57b187f201b5dc
yunhozz <qkrdbsgh1121@naver.com>
2024-01-07 16:47:45 +0900

    fix: API 응답 객체 수정
    * ResponseEntity -> ResponseDTO

--- music-service/src/main/java/com/muco/musicservice/interfaces/MusicController.java
+++ music-service/src/main/java/com/muco/musicservice/interfaces/MusicController.java
@@ -23,6 +23,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
@@ -35,7 +36,8 @@ public class MusicController {
     private final MusicService musicService;
 
     @PostMapping
-    public ResponseEntity<ResponseDTO<Long>> createMusic(@Valid @RequestBody CreateMusicSimpleRequestDTO dto) throws JsonProcessingException {
+    @ResponseStatus(HttpStatus.CREATED)
+    public ResponseDTO createMusic(@Valid @RequestBody CreateMusicSimpleRequestDTO dto) throws JsonProcessingException {
         String userInfoRequestUri = UriComponentsBuilder
                 .fromUriString("http://localhost:8000/api/users/{id}")
                 .build()
@@ -43,10 +45,10 @@ public class MusicController {
                 .encode().toString();
 
         ResponseEntity<String> response = new RestTemplate().getForEntity(userInfoRequestUri, String.class);
-        ResponseDTO<UserInfoClientDTO> responseDTO = new ObjectMapper()
-                .readValue(response.getBody().toString(), new TypeReference<>() {});
+        ObjectMapper objectMapper = new ObjectMapper();
+        ResponseDTO responseDTO = objectMapper.readValue(response.getBody(), new TypeReference<>() {});
+        UserInfoClientDTO userInfo = objectMapper.convertValue(responseDTO.getData(), UserInfoClientDTO.class);
 
-        UserInfoClientDTO userInfo = responseDTO.getData();
         CreateMusicRequestDTO createMusicRequestDTO = CreateMusicRequestDTO.builder()
                 .userId(Long.parseLong(userInfo.getId()))
                 .email(userInfo.getEmail())
@@ -60,32 +62,40 @@ public class MusicController {
 
         Long musicId = musicService.registerMusic(createMusicRequestDTO);
 
-        return ResponseEntity.status(HttpStatus.CREATED)
-                .body(ResponseDTO.of("음악을 성공적으로 등록하였습니다.", musicId));
+        return ResponseDTO.of("음악을 성공적으로 등록하였습니다.", musicId, Long.class);
     }
 
+    /**
+     * TODO: 키워드 검색
+     * 통합 검색: 각 조건 별 N건 조회
+     * 뮤지션: 성별, 국적, 장르 별 조회
+     * 곡: 장르 별 N건 조회
+     * 가사: 정확도, 최신순, 가나다순 조회
+     */
     @GetMapping("/search")
-    public ResponseEntity<ResponseDTO<SearchResponseDTO>> getMusicListByKeyword(
+    @ResponseStatus(HttpStatus.OK)
+    public ResponseDTO getMusicListByKeyword(
         @RequestParam(required = false, defaultValue = "total") String category,
         @RequestParam String keyword
     ) {
         SearchCategory searchCategory = SearchCategory.of(category);
-        SearchResponseDTO searchResponseDTO = null;
+        SearchResponseDTO data = null;
 
         switch (searchCategory) {
-            case TOTAL -> searchResponseDTO = musicService.getMusicListByMusicNameSearch(keyword);
+            case TOTAL -> data = musicService.getMusicListByMusicNameSearch(keyword);
             // TODO: 곡, 아티스트, 가사 검색 추가
         }
 
-        return ResponseEntity.ok(ResponseDTO.of("키워드 검색 결과입니다.", searchResponseDTO));
+        return ResponseDTO.of("키워드 검색 결과입니다.", data, SearchResponseDTO.class);
     }
 
     @PostMapping("/chart")
-    public ResponseEntity<ResponseDTO<Slice<MusicChartQueryDTO>>> getMusicChart(
+    @ResponseStatus(HttpStatus.OK)
+    public ResponseDTO getMusicChart(
         @RequestParam(required = false) Integer cursorRank,
         @PageableDefault(size = 20) Pageable pageable)
     {
-        Slice<MusicChartQueryDTO> musicChartList = musicService.getMusicChartList(cursorRank, pageable);
-        return ResponseEntity.ok(ResponseDTO.of("음악 차트 조회에 성공하였습니다.", musicChartList));
+        Slice<MusicChartQueryDTO> data = musicService.getMusicChartList(cursorRank, pageable);
+        return ResponseDTO.of("음악 차트 조회 결과입니다.", data, Slice.class);
     }
 }
