379f16a52056214757938a5ab2ea583f827bb533
yunhozz <qkrdbsgh1121@naver.com>
2024-01-10 17:21:33 +0900

    fix: 키워드 검색 및 카테고리 별 키워드 검색 엔드포인트 수정

--- music-service/src/main/java/com/muco/musicservice/interfaces/MusicController.java
+++ music-service/src/main/java/com/muco/musicservice/interfaces/MusicController.java
@@ -5,10 +5,12 @@ import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.muco.musicservice.application.MusicService;
 import com.muco.musicservice.global.dto.request.CreateMusicRequestDTO;
+import com.muco.musicservice.global.dto.request.SearchRequestDTO;
 import com.muco.musicservice.global.dto.response.MusicListResponseDTO;
 import com.muco.musicservice.global.dto.response.SearchResponseDTO;
 import com.muco.musicservice.global.dto.response.query.MusicChartQueryDTO;
 import com.muco.musicservice.global.enums.SearchCategory;
+import com.muco.musicservice.global.enums.SearchOrder;
 import com.muco.musicservice.interfaces.dto.CreateMusicSimpleRequestDTO;
 import com.muco.musicservice.interfaces.dto.ResponseDTO;
 import com.muco.musicservice.interfaces.dto.UserInfoClientDTO;
@@ -20,6 +22,7 @@ import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -70,15 +73,11 @@ public class MusicController {
     }
 
     /**
-     * TODO: 키워드 검색
-     * 통합 검색: 각 조건 별 N건 조회
-     * 뮤지션: 성별, 국적, 장르 별 조회
-     * 곡: 장르 별 N건 조회
-     * 가사: 정확도, 최신순, 가나다순 조회
+     * 키워드 검색
      */
-    @GetMapping("/search")
+    @GetMapping("/search/{category}")
     @ResponseStatus(HttpStatus.OK)
-    public ResponseDTO getMusicListByKeyword(@RequestParam(required = false, defaultValue = "total") String category, @RequestParam String keyword) {
+    public ResponseDTO getMusicListByKeyword(@PathVariable(required = false) String category, @RequestParam String keyword) {
         SearchCategory searchCategory = SearchCategory.of(category);
         SearchResponseDTO dataByCategory = null;
         List<SearchResponseDTO> dataByCategories = new ArrayList<>();
@@ -96,6 +95,24 @@ public class MusicController {
                 return ResponseDTO.of("키워드 검색 결과가 존재하지 않습니다.");
             }
         }
+    }
+
+    /**
+     * TODO: 카테고리 별 키워드 검색
+     * 통합 검색: 각 조건 별 N건 조회
+     * 뮤지션: 성별, 국적, 장르 별 조회
+     * 곡: 장르 별 N건 조회
+     * 가사: 정확도, 최신순, 가나다순 조회
+     */
+    @PostMapping("/search/{category}")
+    public ResponseDTO getSearchResultsByKeywordAndCategory(@PathVariable String category, @Valid @RequestBody SearchRequestDTO dto, Pageable pageable) {
+        SearchCategory searchCategory = SearchCategory.of(category);
+
+        switch (searchCategory) {
+            case MUSIC -> musicService.getMusicSearchPageBySearchConditions(dto, pageable);
+        }
+
+        return ResponseDTO.of("키워드 검색 결과입니다.");
     }
 
     @PostMapping("/chart")
