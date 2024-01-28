8c06021f3900c62bdae6f3bb05b9611c128a09f6
yunhozz <qkrdbsgh1121@naver.com>
2024-01-28 22:26:42 +0900

    add: 파일 저장을 위한 컬럼 추가

--- music-service/src/main/java/com/muco/musicservice/domain/persistence/entity/Music.java
+++ music-service/src/main/java/com/muco/musicservice/domain/persistence/entity/Music.java
@@ -10,14 +10,19 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.Where;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
 @Entity
 @Getter
+@Where(clause = "deleted_at is null")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Music extends BaseEntity {
 
@@ -34,25 +39,46 @@ public class Music extends BaseEntity {
     @Column(columnDefinition = "TEXT")
     private String lyrics;
 
+    @ColumnDefault("0")
     private int playCount;
 
+    @ColumnDefault("0")
     private int likeCount;
 
+    @ColumnDefault("-1")
     private int ranking;
+
+    private String originalName;
+
+    private String savedName;
+
+    private String fileUrl;
 
     private String imageUrl;
 
-    private Music(String name, List<Genre> genres, String lyrics, int playCount, int likeCount, int ranking, String imageUrl) {
+    @ColumnDefault("null")
+    private LocalDateTime deletedAt;
+
+    @Builder(access = AccessLevel.PRIVATE)
+    private Music(String name, List<Genre> genres, String lyrics, String originalName, String savedName, String fileUrl, String imageUrl) {
         this.name = name;
         this.genres = genres;
         this.lyrics = lyrics;
-        this.playCount = playCount;
-        this.likeCount = likeCount;
-        this.ranking = ranking;
+        this.originalName = originalName;
+        this.savedName = savedName;
+        this.fileUrl = fileUrl;
         this.imageUrl = imageUrl;
     }
 
-    public static Music create(String name, List<Genre> genres, String lyrics, String imageUrl) {
-        return new Music(name, genres, lyrics, 0, 0, -1, imageUrl);
+    public static Music create(String name, List<Genre> genres, String lyrics, String originalName, String savedName, String fileUrl, String imageUrl) {
+        return Music.builder()
+                .name(name)
+                .genres(genres)
+                .lyrics(lyrics)
+                .originalName(originalName)
+                .savedName(savedName)
+                .fileUrl(fileUrl)
+                .imageUrl(imageUrl)
+                .build();
     }
 }
