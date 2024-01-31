4f111d093cfffb754e4a301a2d03b8fb0dc1f235
yunhozz <qkrdbsgh1121@naver.com>
2024-01-31 16:06:27 +0900

    improve: FileHandler 추상 멤버 및 메소드 보완

--- music-service/src/main/java/com/muco/musicservice/domain/application/handler/FileHandler.java
+++ music-service/src/main/java/com/muco/musicservice/domain/application/handler/FileHandler.java
@@ -6,21 +6,34 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Path;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.UUID;
 
 public abstract class FileHandler<E extends BaseEntity, T> {
 
+    protected String originalName;
+    protected String savedName;
     protected String fileUrl;
 
-    protected void createSavedPath() {
+    protected void transferFile(MultipartFile file) throws IOException {
+        originalName = file.getOriginalFilename();
+        String extension = originalName.substring(originalName.lastIndexOf("."));
+        String uuid = UUID.randomUUID().toString();
+        savedName = uuid + extension;
+
         String absolutePath = new File("").getAbsolutePath() + "\\";
         SimpleDateFormat dateFormat = new SimpleDateFormat("yyMMddHHmmss");
         String currentDate = dateFormat.format(new Date());
         fileUrl = absolutePath + "/" + currentDate;
+
+        file.transferTo(new File(fileUrl + "/" + savedName));
     }
 
-    protected abstract E upload(MultipartFile file, T in) throws IOException;
-    protected abstract Resource download(String fileName) throws IOException;
-    protected abstract Resource display(String fileName);
+    public abstract E upload(MultipartFile file, T in) throws IOException;
+    public abstract Resource download(String fileName) throws IOException;
+    public abstract Resource display(String fileName) throws IOException;
+    public abstract String createContentType(String fileName) throws IOException;
+    protected abstract Path getPath(String name);
 }
--- music-service/src/main/java/com/muco/musicservice/domain/application/handler/MusicFileHandler.java
+++ music-service/src/main/java/com/muco/musicservice/domain/application/handler/MusicFileHandler.java
@@ -2,34 +2,34 @@ package com.muco.musicservice.domain.application.handler;
 
 import com.muco.musicservice.domain.persistence.entity.Music;
 import com.muco.musicservice.global.dto.request.CreateMusicRequestDTO;
-import org.springframework.core.io.FileSystemResource;
 import org.springframework.core.io.InputStreamResource;
 import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.UUID;
 
 @Component
-public class MusicFileHandler extends FileHandler<Music, CreateMusicRequestDTO> {
+public final class MusicFileHandler extends FileHandler<Music, CreateMusicRequestDTO> {
+
+    private MusicFileHandler() {}
 
     @Override
     public Music upload(MultipartFile file, CreateMusicRequestDTO dto) throws IOException {
-        String originalName = file.getOriginalFilename();
-        String extension = originalName.substring(originalName.lastIndexOf("."));
-        String uuid = UUID.randomUUID().toString();
-        String savedName = uuid + extension;
-
-        createSavedPath();
-        file.transferTo(new File(fileUrl + "/" + savedName));
-
-        return Music.create(dto.musicName(), dto.genres(), dto.lyrics(), originalName, savedName, fileUrl, null);
+        super.transferFile(file);
+        return Music.create(
+                dto.musicName(),
+                dto.genres(),
+                dto.lyrics(),
+                originalName,
+                savedName,
+                fileUrl,
+                null
+        );
     }
 
     @Override
@@ -39,18 +39,20 @@ public class MusicFileHandler extends FileHandler<Music, CreateMusicRequestDTO> 
         return new InputStreamResource(inputStream);
     }
 
+    //TODO
     @Override
-    public Resource display(String fileName) {
-        Path path = getPath(fileName);
-        return new FileSystemResource(path);
+    public Resource display(String fileName) throws IOException {
+        return null;
     }
 
+    @Override
     public String createContentType(String fileName) throws IOException {
         Path path = getPath(fileName);
         return Files.probeContentType(path);
     }
 
-    private Path getPath(String name) {
+    @Override
+    protected Path getPath(String name) {
         return Paths.get(fileUrl + "/" + name);
     }
 }
