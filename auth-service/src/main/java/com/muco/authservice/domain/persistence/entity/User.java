3cabab2b7dcc5af86a96f77bd57e1fba7294137c
yunhozz <qkrdbsgh1121@naver.com>
2024-02-07 15:55:12 +0900

    fix: enum type(=Role) 컬렉션 타입으로 수정 #5

--- auth-service/src/main/java/com/muco/authservice/domain/persistence/entity/User.java
+++ auth-service/src/main/java/com/muco/authservice/domain/persistence/entity/User.java
@@ -2,12 +2,16 @@ package com.muco.authservice.domain.persistence.entity;
 
 import com.muco.authservice.global.enums.LoginType;
 import com.muco.authservice.global.enums.Role;
+import jakarta.persistence.CollectionTable;
+import jakarta.persistence.ElementCollection;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.Getter;
@@ -29,6 +33,8 @@ public class User extends BaseEntity {
     @Enumerated(EnumType.STRING)
     private LoginType loginType; // GOOGLE, KAKAO, NAVER, LOCAL
 
+    @ElementCollection(fetch = FetchType.LAZY, targetClass = Role.class)
+    @CollectionTable(joinColumns = @JoinColumn(name = "user_id"), name = "user_role")
     @Enumerated(EnumType.STRING)
     private Set<Role> roles = new HashSet<>(); // ADMIN, USER, GUEST
 
