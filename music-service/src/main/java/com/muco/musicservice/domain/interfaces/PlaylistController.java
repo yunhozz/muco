7f59b7d70d883a85abefe05f74a8aaec177ff802
yunhozz <qkrdbsgh1121@naver.com>
2024-02-20 16:17:49 +0900

    modify: Add Response Status on each API #10

--- music-service/src/main/java/com/muco/musicservice/domain/interfaces/PlaylistController.java
+++ music-service/src/main/java/com/muco/musicservice/domain/interfaces/PlaylistController.java
@@ -6,6 +6,7 @@ import com.muco.musicservice.global.dto.request.CreatePlaylistRequestDTO;
 import com.muco.musicservice.global.dto.request.UpdatePlaylistRequestDTO;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -24,30 +26,35 @@ public class PlaylistController {
     private final PlaylistService playlistService;
 
     @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
     public ResponseDTO createPlaylist(@RequestHeader String sub, @Valid @RequestBody CreatePlaylistRequestDTO dto) {
         Long playlistId = playlistService.createPlaylist(sub, dto);
         return ResponseDTO.of("플레이리스트 생성이 완료되었습니다.", playlistId, Long.class);
     }
 
     @PutMapping
+    @ResponseStatus(HttpStatus.CREATED)
     public ResponseDTO addMusicOnPlaylist(@RequestParam String id, @RequestParam String... musicIds) {
         playlistService.addMusicOnPlaylist(id, musicIds);
         return ResponseDTO.of("음원을 성공적으로 추가하였습니다.");
     }
 
     @PatchMapping
+    @ResponseStatus(HttpStatus.OK)
     public ResponseDTO updatePlaylist(@RequestParam String playlistId, @Valid @RequestBody UpdatePlaylistRequestDTO dto) {
         playlistService.updatePlaylist(playlistId, dto);
         return ResponseDTO.of("해당 플레이리스트에 대한 업데이트를 완료했습니다.");
     }
 
     @DeleteMapping("/music")
+    @ResponseStatus(HttpStatus.OK)
     public ResponseDTO deleteMusicFromPlaylist(@RequestParam String playlistId, @RequestParam String... musicIds) {
         playlistService.deleteMusicFromPlaylist(playlistId, musicIds);
         return ResponseDTO.of("음원을 성공적으로 제거하였습니다.");
     }
 
     @DeleteMapping
+    @ResponseStatus(HttpStatus.OK)
     public ResponseDTO deletePlaylist(@RequestParam String id) {
         playlistService.deletePlaylist(id);
         return ResponseDTO.of("해당 플레이리스트를 성공적으로 제거하였습니다.");
