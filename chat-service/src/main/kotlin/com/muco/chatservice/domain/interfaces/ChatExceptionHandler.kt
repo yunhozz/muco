e0d5386fc95a9c538728c650aee35c329519e25f
yunhozz <qkrdbsgh1121@naver.com>
2024-03-14 20:24:02 +0900

    FEAT: create global exception handler on chat service #11

---
+++ chat-service/src/main/kotlin/com/muco/chatservice/domain/interfaces/ChatExceptionHandler.kt
@@ -0,0 +1,39 @@
+package com.muco.chatservice.domain.interfaces
+
+import com.muco.chatservice.domain.interfaces.dto.ErrorResponseDTO
+import com.muco.chatservice.global.enums.ErrorCode
+import com.muco.chatservice.global.util.logger
+import org.springframework.http.HttpStatus
+import org.springframework.web.bind.MethodArgumentNotValidException
+import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.bind.annotation.ResponseStatus
+import org.springframework.web.bind.annotation.RestControllerAdvice
+import org.springframework.web.client.HttpClientErrorException
+import reactor.core.publisher.Mono
+
+@RestControllerAdvice
+class ChatExceptionHandler {
+
+    val log = logger()
+
+    @ExceptionHandler(Exception::class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    fun handleException(e: Exception): Mono<ErrorResponseDTO> {
+        log.error(e.localizedMessage)
+        return ErrorResponseDTO.of(ErrorCode.INTERNAL_SERVER_ERROR, e.localizedMessage)
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException::class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    fun handleMethodArgumentNotValidException(e: MethodArgumentNotValidException): Mono<ErrorResponseDTO> {
+        log.error(e.localizedMessage)
+        return ErrorResponseDTO.of(ErrorCode.INVALID_REQUEST, e.bindingResult)
+    }
+
+    @ExceptionHandler(HttpClientErrorException::class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    fun handleHttpClientErrorException(e: HttpClientErrorException): Mono<ErrorResponseDTO> {
+        log.error(e.localizedMessage)
+        return Mono.justOrEmpty(e.getResponseBodyAs(ErrorResponseDTO::class.java))
+    }
+}
