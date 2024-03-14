678ffd14498dd82db718a399e18b7b6af4813c41
yunhozz <qkrdbsgh1121@naver.com>
2024-03-14 20:22:34 +0900

    ADD: add field partnerId on creating chatroom DTO #11

--- chat-service/src/main/kotlin/com/muco/chatservice/global/dto/request/CreateChatroomRequestDTO.kt
+++ chat-service/src/main/kotlin/com/muco/chatservice/global/dto/request/CreateChatroomRequestDTO.kt
@@ -1,5 +1,11 @@
 package com.muco.chatservice.global.dto.request
 
+import jakarta.validation.constraints.NotBlank
+import jakarta.validation.constraints.NotNull
+
 data class CreateChatroomRequestDTO(
+    @NotNull
+    val partnerId: Long,
+    @NotBlank
     val name: String
 )
