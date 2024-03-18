e133564d27c6d2591cf79cce2d02628f2309d1fd
yunhozz <qkrdbsgh1121@naver.com>
2024-03-18 15:53:05 +0900

    FIX: add annotations for serialization for type definition error #11

--- chat-service/src/main/kotlin/com/muco/chatservice/global/dto/request/CreateChatroomRequestDTO.kt
+++ chat-service/src/main/kotlin/com/muco/chatservice/global/dto/request/CreateChatroomRequestDTO.kt
@@ -1,11 +1,15 @@
 package com.muco.chatservice.global.dto.request
 
+import com.fasterxml.jackson.annotation.JsonProperty
 import jakarta.validation.constraints.NotBlank
 import jakarta.validation.constraints.NotNull
 
 data class CreateChatroomRequestDTO(
+    @JsonProperty("partnerId")
     @field:NotNull
     val partnerId: Long?,
+
+    @JsonProperty("name")
     @field:NotBlank
     val name: String?
 )
