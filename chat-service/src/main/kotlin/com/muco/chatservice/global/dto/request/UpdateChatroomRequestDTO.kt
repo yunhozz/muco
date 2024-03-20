22cf844494da171032d1d4c2523270820410527d
yunhozz <qkrdbsgh1121@naver.com>
2024-03-20 21:34:19 +0900

    ADD: add validation #11

--- chat-service/src/main/kotlin/com/muco/chatservice/global/dto/request/UpdateChatroomRequestDTO.kt
+++ chat-service/src/main/kotlin/com/muco/chatservice/global/dto/request/UpdateChatroomRequestDTO.kt
@@ -1,5 +1,10 @@
 package com.muco.chatservice.global.dto.request
 
+import com.fasterxml.jackson.annotation.JsonProperty
+import jakarta.validation.constraints.NotBlank
+
 data class UpdateChatroomRequestDTO(
+    @JsonProperty("name")
+    @field:NotBlank
     val name: String
 )
