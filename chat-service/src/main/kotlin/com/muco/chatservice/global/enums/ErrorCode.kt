ef827ecef632343b25e3fbc285c4ba3bcb0e3769
yunhozz <qkrdbsgh1121@naver.com>
2024-03-24 19:30:37 +0900

    STYLE: change error code #11

--- chat-service/src/main/kotlin/com/muco/chatservice/global/enums/ErrorCode.kt
+++ chat-service/src/main/kotlin/com/muco/chatservice/global/enums/ErrorCode.kt
@@ -5,11 +5,11 @@ enum class ErrorCode(
     val code: String,
     val message: String
 ) {
-    INVALID_REQUEST(400, "M-001", "Invalid request"),
-    INVALID_VALUE_TYPES(400, "M-002", "Invalid value types"),
-    RESOURCE_NOT_FOUND(404, "M-004", "Resource not found"),
-    METHOD_NOT_ALLOWED(405, "M-005", "Invalid Method"),
-    PAYLOAD_TOO_LARGE(413, "M-013", "File size exceeds maximum limit"),
-    UNSUPPORTED_MEDIA_TYPE(415, "M-015", "Media type not supported"),
-    INTERNAL_SERVER_ERROR(500, "M-500", "Server Error");
+    INVALID_REQUEST(400, "C-001", "Invalid request"),
+    INVALID_VALUE_TYPES(400, "C-002", "Invalid value types"),
+    RESOURCE_NOT_FOUND(404, "C-004", "Resource not found"),
+    METHOD_NOT_ALLOWED(405, "C-005", "Invalid Method"),
+    PAYLOAD_TOO_LARGE(413, "C-013", "File size exceeds maximum limit"),
+    UNSUPPORTED_MEDIA_TYPE(415, "C-015", "Media type not supported"),
+    INTERNAL_SERVER_ERROR(500, "C-500", "Server Error");
 }
